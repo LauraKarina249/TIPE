7eef7e26fb36f49319401bfd9b8f6bbaba4f1551
LauraKarina249 <40294571+LauraKarina249@users.noreply.github.com>
2018-06-15 12:54:36 +0200

    petit commentaire

--- TIPE.py
+++ TIPE.py
@@ -38,7 +38,7 @@ def nouvelle_position(W,oscillations,i,j,n,D,rho,h,dl,dt):
         if j == Nlongueur-1 or j == Nlongueur-0:
             j = j-2   
          
-         
+        # Ne pas oublier le carré du dt**2 
         return (1/(2*rho*h*(dl**4)-16*D*(dt**2)))*((rho*h*(dl**4))*(W[n+1,i,j]+W[n-1,i,j])+(D*dt)*(W[n,i+2,j]+W[n,i-2,j]+W[n,i,j+2]+W[n,i,j-2]-6*(W[n,i+1,j]+W[n,i-1,j]+W[n,i,j+1]+W[n,i,j-1])+W[n,i+1,j+1]+W[n,i+1,j-1]+W[n,i-1,j+1]+W[n,i-1,j-1]))
          
      
